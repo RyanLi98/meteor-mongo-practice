edb6ae03c09ec803d8558011d2feee78612bf61e
Ryan Li <ryanli80@hawaii.edu>
2019-03-06 23:20:21 -1000

    simple-schema

--- app/both/PeopleCollection.js
+++ app/both/PeopleCollection.js
@@ -1,3 +1,15 @@
 import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
 
 export const people = new Mongo.Collection('People');
+
+
+const peopleSchema = new SimpleSchema(
+    {
+      first: String,
+      last: String,
+      age: SimpleSchema.Integer,
+      city: String
+    }
+);
+people.attachSchema(peopleSchema);
